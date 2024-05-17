53003e26c46cf3c61d5f938dedb3858e44958d37
Zainds <beat1097@mail.ru>
2024-05-17 21:30:51 +0700

    junk code removed

--- app/src/main/java/com/utsman/osmapp/botton_navigation/BottomItem.kt
+++ app/src/main/java/com/utsman/osmapp/botton_navigation/BottomItem.kt
@@ -1,8 +1,7 @@
 package com.utsman.osmapp.botton_navigation
 
-import com.utsman.osmandcompose.R
 
 sealed class BottomItem(val title: String, val iconId: Int, val route: String) {
     object Screen1: BottomItem("Карта", com.utsman.osmapp.R.drawable.gps_dot, "screen_1")
-    object Screen2: BottomItem("Карта", com.utsman.osmapp.R.drawable.account, "screen_2")
+    object Screen2: BottomItem("Профиль", com.utsman.osmapp.R.drawable.account, "screen_2")
 }
